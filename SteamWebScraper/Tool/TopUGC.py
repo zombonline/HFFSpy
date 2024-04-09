107bacf995b81c42e2bdc3b7fede7371bc9b7a1f
Bradley's Laptop <megazombie130@gmail.com>
2024-04-09 15:45:40 +0100

    all scans in seperate files settings added creator page started

--- SteamWebScraper/Tool/TopUGC.py
+++ SteamWebScraper/Tool/TopUGC.py
@@ -16,35 +16,9 @@ import xlsxwriter
 import math
 from decouple import config
 from enum import Enum
+import data_functions
 
-class CreatorStatus(Enum):
-    signed_creator = 1
-    contacted_creator = 2
-    pending_contact_creator = 3
-    standard_creator = 4
-class WorkshopItem:
-    def __init__(self, title, creator_name, country, language, tus, rating, comment_count, date_posted, creator_status):
-        self.title = title
-        self.creator_name = creator_name
-        self.date_posted = date_posted
-        self.country = country
-        self.language = language 
-        self.tus = tus
-        self.rating = rating
-        self.comment_count = comment_count
-        self.creator_status = creator_status    
-def get_item_comment_count(item, driver, wait):
-    item_url = item.find_element(By.CSS_SELECTOR, 'a').get_attribute('href')
-    driver.execute_script(f"window.open('{item_url}');")
-    driver.switch_to.window(driver.window_handles[1])
-    try:
-        wait.until(EC.presence_of_element_located((By.CLASS_NAME, "commentthread_count_label")))
-        comment_count = driver.find_element(By.CLASS_NAME, "commentthread_count_label").text.split(' ')[0]
-    except TimeoutException:
-        comment_count = "0"
-    driver.close()
-    driver.switch_to.window(driver.window_handles[0])
-    return comment_count
+
 def output_to_excel(level_list, model_list):
     workbook = xlsxwriter.Workbook('UGCOfWorkshopMonth.xlsx')
     worksheet = workbook.add_worksheet()
@@ -73,7 +47,7 @@ def output_to_excel(level_list, model_list):
         worksheet.write(i+2, 0, i+1)
         worksheet.write(i+2, 1, level_list[i].title)
         worksheet.write(i+2, 2, level_list[i].creator_name)
-        worksheet.write(i+2, 3, level_list[i].date_posted.strftime("%m/%d/%Y, %H:%M:%S"))
+        worksheet.write(i+2, 3, level_list[i].date_posted)
         worksheet.write(i+2, 4, level_list[i].country)
         worksheet.write(i+2, 5, level_list[i].language)
         worksheet.write(i+2, 6, level_list[i].tus)
@@ -83,113 +57,42 @@ def output_to_excel(level_list, model_list):
         worksheet.write(i+24, 0, i+1)
         worksheet.write(i+24, 1, model_list[i].title)
         worksheet.write(i+24, 2, model_list[i].creator_name)
-        worksheet.write(i+24, 3, model_list[i].date_posted.strftime("%m/%d/%Y, %H:%M:%S"))
+        worksheet.write(i+24, 3, model_list[i].date_posted)
         worksheet.write(i+24, 4, model_list[i].country)
         worksheet.write(i+24, 5, model_list[i].language)
         worksheet.write(i+24, 6, model_list[i].tus)
         worksheet.write(i+24, 7, model_list[i].rating)
         worksheet.write(i+24, 8, model_list[i].comment_count)
-    workbook.close()
-    
-def ExtractWorkShopData(driver, wait):
-    wait.until(EC.presence_of_element_located((By.CLASS_NAME, "workshopItem")))
-    workshopItems = driver.find_elements(By.CLASS_NAME, "workshopItem")
-    if(len(workshopItems) > 20):
-        workshopItems = workshopItems[:20]
-    workshop_data = []
-    for item in workshopItems:
-        params = {
-                'key': KEY,
-                'itemcount': 1,
-                'publishedfileids[0]': item.find_element(By.CSS_SELECTOR, 'a').get_attribute('data-publishedfileid')
-            }
-        response = requests.post(url, data=params)
-        item_data = response.json()
-        print(steam.users.get_user_details(item_data['response']['publishedfiledetails'][0]['creator']))
-        item_title = item_data['response']['publishedfiledetails'][0]['title']
-        item_creator_name = steam.users.get_user_details(item_data['response']['publishedfiledetails'][0]['creator'])['player']['personaname']
-        item_TUS = item_data['response']['publishedfiledetails'][0]['lifetime_subscriptions']
-        item_rating = 'N/A (Needs log in)'
-        item_comment_count = get_item_comment_count(item, driver, wait)
-        item_date_posted = datetime.fromtimestamp(item_data['response']['publishedfiledetails'][0]['time_created'])
-        creatorID = item_data['response']['publishedfiledetails'][0]['creator']
-        item_creator_status = GetCreatorStatus(creatorID)
-        item_creator_country = get_user_country(steam.users.get_user_details(creatorID))
-        item_creator_language = get_langauge_from_user_and_item(steam.users.get_user_details(creatorID), item_data['response']['publishedfiledetails'][0])
-        item = WorkshopItem(item_title, item_creator_name, item_creator_country, item_creator_language, item_TUS, item_rating, item_comment_count, item_date_posted, item_creator_status)
-        workshop_data.append(item)
-    return workshop_data
-def GetCreatorStatus(creator_ID):
-    this_dir = os.path.dirname(os.path.abspath(__file__))
+    workbook.close() 
 
-    file = open(f"{this_dir}/CreatorStatus.txt", "r")
-    for line in file:
-        if creator_ID in line:
-            try:
-                return CreatorStatus(int(line.split('- ')[1]))
-            except:
-                return CreatorStatus(4)
-def get_user_country(user):
-    if('loccountrycode' in user['player']):
-        return user['player']['loccountrycode']
-    else:
-        return "N/A"
-def get_langauge_from_user_and_item(user, workshop_item):
-    #chinese override, if the user's country is already display as China, then the language is Chinese
-    if(get_user_country(user) == 'CN'):
-        return 'zh-cn'
-    strings = []
-    if user is None:
-        pass
-    else:
-        if('personaname' in user['player']):
-            strings.append(user['player']['personaname'])
-        if('realname' in user['player']):
-            strings.append(user['player']['realname'])
-        if('bio' in user['player']):
-            strings.append(user['player']['bio'])
-    if workshop_item is None:
-        pass
-    else:
-        if('title' in workshop_item):
-            strings.append(workshop_item['title'])
-        if('description' in workshop_item):
-            strings.append(workshop_item['description'])
-    newString = ""
-    for string in strings:
-        newString = newString + ", " + string
-    try:
-        detected_language = detect(newString)
-    except:
-        detected_language = "N/A"
-
-    #Chinese override (Example: if a player has a chinese username, but is using english in title/description of the item,
-    #the language might be detected as english, but we want to override that and set it to chinese)
-    if 'zh' not in detected_language:
-        for string in strings:
-            try:
-                if 'zh' in detect(string):
-                    detected_language = 'zh-cn'
-                    break
-            except:
-                continue
-    return detected_language
-
+def create_workshop_item_objects_array(item_ids):
+    item_objects = []
+    for item_id in item_ids:
+        item_object = data_functions.create_workshop_item(item_id, driver)
+        item_objects.append(item_object)
+    return item_objects
 KEY = config('STEAM_API_KEY')
 steam = Steam(KEY)
 url = f"https://api.steampowered.com/ISteamRemoteStorage/GetPublishedFileDetails/v1/"   
-        
-def scan(driver, wait, start_date, end_date, amount_of_items):
-    print("Scanning for top UGC of the month...")
-    print(f"From {start_date} to {end_date}")
-    print(f"Amount of items to scan: {amount_of_items}")
-    level_link = f"https://steamcommunity.com/workshop/browse/?appid=477160&browsesort=trend&section=readytouseitems&requiredtags%5B0%5D=Levels&created_date_range_filter_start={start_date}&created_date_range_filter_end={end_date}&updated_date_range_filter_start=NaN&updated_date_range_filter_end=NaN&actualsort=trend&p=1&days=-1"
+driver = None
+wait = None
+def scan(d, start_date, end_date, amount_of_items):
+    global driver
+    driver = d
+    global wait
+    wait = WebDriverWait(driver, 10)
+    level_link = f"https://steamcommunity.com/workshop/browse/?appid=477160&browsesort=trend&section=readytouseitems&requiredtags%5B0%5D=Levels&created_date_range_filter_start={start_date}&created_date_range_filter_end={end_date}&updated_date_range_filter_start=NaN&updated_date_range_filter_end=NaN&actualsort=trend&p=1"
     driver.get(level_link)
-    level_workshop_data = ExtractWorkShopData(driver, wait)
 
-    model_link = f"https://steamcommunity.com/workshop/browse/?appid=477160&browsesort=trend&section=readytouseitems&requiredtags%5B0%5D=Model&created_date_range_filter_start={start_date}&created_date_range_filter_end={end_date}&updated_date_range_filter_start=NaN&updated_date_range_filter_end=NaN&actualsort=trend&p=1&days=-1"
+    item_ids = data_functions.get_item_ids(driver, amount_of_items)
+    level_workshop_data = create_workshop_item_objects_array(item_ids)
+
+    model_link = f"https://steamcommunity.com/workshop/browse/?appid=477160&browsesort=trend&section=readytouseitems&requiredtags%5B0%5D=Model&created_date_range_filter_start={start_date}&created_date_range_filter_end={end_date}&updated_date_range_filter_start=NaN&updated_date_range_filter_end=NaN&actualsort=trend&p=1"
     driver.get(model_link)
-    model_workshop_data = ExtractWorkShopData(driver, wait)
+
+    item_ids = data_functions.get_item_ids(driver, amount_of_items)
+    model_workshop_data = create_workshop_item_objects_array(item_ids)
+    model_workshop_data = create_workshop_item_objects_array(item_ids)
     output_to_excel(level_workshop_data, model_workshop_data)
 
 
--- SteamWebScraper/Tool/TotalUGC.py
+++ SteamWebScraper/Tool/TotalUGC.py
@@ -14,28 +14,8 @@ import xlsxwriter
 from steam import Steam
 from decouple import config
 
-class Game:
-    def __init__(self, appID, name, amountOfItems):
-        self.appID = appID
-        self.name = name
-        self.amountOfItems = amountOfItems
+import data_functions
 
-def CreateGameObject(gameAppID, driver, wait):
-    link = "https://steamcommunity.com/workshop/browse/?appid=" + gameAppID
-    driver.get(link)
-    gameName = steam.apps.get_app_details(gameAppID)[gameAppID]['data']['name']
-    gameWorkShopItemCount = "N/A"
-    if gameAppID in driver.current_url:
-        try:
-            wait.until(lambda driver: len(driver.find_elements(By.CSS_SELECTOR, 'div.workshopBrowsePagingInfo')) > 0)
-            itemCountText = driver.find_element(By.CSS_SELECTOR, 'div.workshopBrowsePagingInfo').text
-            gameWorkShopItemCount = int(itemCountText.split('of ')[1].split(' entries')[0].replace(',', ''))
-        except TimeoutException:
-            print("Timed out waiting for page to load")
-            return
-
-    newGame = Game(gameAppID, gameName, gameWorkShopItemCount)
-    return newGame
 
 def WriteToExcel(games):
     workbook = xlsxwriter.Workbook('UGCOfMajorGames.xlsx')
@@ -51,27 +31,35 @@ def WriteToExcel(games):
     
     workbook.close()
 
-
 KEY = config("STEAM_API_KEY")
 steam = Steam(KEY)
+driver = None
+wait = None
 
-def scan_for_total_UGC_count(driver, wait):
-    listOfAppIDS = []
-    games = []
-
-    #get top 100 games with workshop
+def get_top_100_games_via_steamDB():
     driver.get("https://steamdb.info/charts/?category=30")
     wait.until(lambda driver: len(driver.find_elements(By.CSS_SELECTOR, 'tr.app')) > 0)
     steamdbGames = driver.find_elements(By.CSS_SELECTOR, 'tr.app')
+    listOfAppIDS = []
     for game in steamdbGames:
         appID = game.get_attribute('data-appid')
-        print(appID)
         listOfAppIDS.append(appID)
-    print("Scanning through " + str(len(listOfAppIDS)) + " appIDs...")
-    if(len(listOfAppIDS) > 0):
-        for i in listOfAppIDS:
-            newGame = CreateGameObject(i, driver, wait)
-            games.append(newGame)
+    return listOfAppIDS
+
+def create_game_items(listOfAppIDS):
+    games = []
+    for appID in listOfAppIDS:
+        game = data_functions.create_game_item(appID, driver)
+        games.append(game)
+    return games
+
+def scan_for_total_UGC_count(d):
+    global driver
+    driver = d
+    global wait
+    wait = WebDriverWait(driver, 10)
+
+    games = create_game_items(get_top_100_games_via_steamDB())  
 
     WriteToExcel(games)
     print("Finished")
