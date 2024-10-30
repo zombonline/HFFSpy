de2958249a3773905ed50774890e03a4a3a6b1b6
Bradley Desktop <megazombie130@gmail.com>
2024-10-30 23:20:10 +0000

    Revert "Fixed issue with Steam Top 100 scans being saved to incorrect location."
    This reverts commit a1d43aba96f3fcb40173380e8316d9310193c955.

--- gen_most_played_workshop_games.py
+++ gen_most_played_workshop_games.py
@@ -1,23 +1,13 @@
-from datetime import datetime
+from datetime import date
 import xlsxwriter
 import main_functions
 import os
-from pathlib import Path
 def output_to_excel(games):
-    dt = datetime.now()
-    documents_path = Path(os.path.expanduser('~')) / 'Documents'
-    sheets_path = documents_path / 'HFFSpy Sheets'
-    if not sheets_path.exists():
-        sheets_path.mkdir()
-
-    filename = f'Steam top 100 Scan {dt.year}-{dt.month}-{dt.day}-{dt.hour}-{dt.minute}-{dt.second}.xlsx'
-    full_path = sheets_path / filename
-
-    workbook = xlsxwriter.Workbook(full_path)
+    workbook = xlsxwriter.Workbook('UGCOfMajorGames.xlsx')
     worksheet = workbook.add_worksheet()
     worksheet.write('A1', 'Rank')
     worksheet.write('B1', 'Game Name')
-    worksheet.write('C1', f'Amount of Workshop Items as of {datetime.today().date()}')
+    worksheet.write('C1', f'Amount of Workshop Items as of {date.today()}')
 
     for i in range(len(games)):
         worksheet.write(i+1, 0, i+1)
@@ -25,7 +15,7 @@ def output_to_excel(games):
         worksheet.write(i+1, 2, games[i].amountOfItems)
     
     workbook.close()
-    os.startfile(full_path)
+    os.startfile('UGCOfMajorGames.xlsx')
 
 def scan(progress_queue):
     progress_queue.put("Getting current top 100 games from SteamDB...")
@@ -38,4 +28,3 @@ def scan(progress_queue):
     progress_queue.put("Outputting to Excel...")
     output_to_excel(games)
     progress_queue.put("")
-
